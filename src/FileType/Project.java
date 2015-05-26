c2dd79bd8b60f8f57d65f6f0089fcc912a1499bc
Oliver <nils-oliver.schliebs@uni-tuebingen.de>
2015-05-26 05:27:00 +0200

    Project erweitert um alle Felder aus der projects.tsv. Getter/Setter bei Project hinzugefügt weil VAADIN das so braucht.
    Dummy-Funktion für den Import der Projekte in die UI eingebaut.
    Zu Testzwecken: UI zeigt ne Liste der verfügbaren Projekte an + reagiert
    auf events.

--- src/FileType/Project.java
+++ src/FileType/Project.java
@@ -1,6 +1,60 @@
 package FileType;
 
-public class Project {
-String Identifier;
-String Type;
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class Project implements Serializable  {
+
+	// Field names as in "projects.tsv"
+	String identifier, name, description, members;
+
+	// Constructor to create one new project
+	public Project(String identifier, String name, String description,
+			String members) {
+		this.identifier = identifier;
+		this.name = name;
+		this.description = description;
+		this.members = members;
+	}
+	
+	
+	// Getters/Setters required by VAADINs data model
+	public String getIdentifier() {
+		return identifier;
+	}
+
+
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getMembers() {
+		return members;
+	}
+
+	public void setMembers(String members) {
+		this.members = members;
+	}
+
+	
+	
+
+	
+	
 }
--- src/com/example/qbic_project1/Qbic_project1UI.java
+++ src/com/example/qbic_project1/Qbic_project1UI.java
@@ -1,14 +1,23 @@
 package com.example.qbic_project1;
 
+import java.util.Arrays;
+
 import javax.servlet.annotation.WebServlet;
+
+import FileType.Project;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.Item;
+import com.vaadin.data.Property;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.util.BeanContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.ListSelect;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -21,19 +30,78 @@ public class Qbic_project1UI extends UI {
 	public static class Servlet extends VaadinServlet {
 	}
 
+	// Method to import the content of "projects.tsv" as VAADIN data model
+	private BeanContainer<String, Project> importProjects() {
+
+		// Container for projects
+		BeanContainer<String, Project> projects = new BeanContainer<String, Project>(
+				Project.class);
+
+		// Define which property (ID,name, Descr, Members) to use as internal ID
+		// -> identifier
+		projects.setBeanIdProperty("identifier");
+
+		// Use dummy data until TSV parser is available
+		Project p1 = new Project("PID01", "Project 1",
+				"Description to Project 1", "A1, A2");
+		Project p2 = new Project("PID02", "Project 2",
+				"Description to Project 2", "B1, B2");
+
+		// Add dummy data
+		projects.addBean(p1);
+		projects.addBean(p2);
+
+		// Return container
+		return projects;
+
+	}
+
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void init(VaadinRequest request) {
-		final VerticalLayout layout = new VerticalLayout();
-		layout.setMargin(true);
-		setContent(layout);
+		// Import data models
+		BeanContainer<String, Project> projects = importProjects();
 
-		Button button = new Button("Click Me");
-		button.addClickListener(new Button.ClickListener() {
-			public void buttonClick(ClickEvent event) {
-				layout.addComponent(new Label("Thank you for clicking me :-)"));
+		// Show imported projects
+		ListSelect ls1 = new ListSelect("My projects", projects);
+		ls1.setItemCaptionMode(ListSelect.ItemCaptionMode.PROPERTY);
+		ls1.setItemCaptionPropertyId("name");
+		ls1.setNullSelectionAllowed(false);
+		Label l1 = new Label("ID: ");
+		Label l2 = new Label("Project name: ");
+		Label l3 = new Label("Description: ");
+		Label l4 = new Label("Members: ");
+
+		// Add listener to ListSelect ls1
+		ls1.addListener(new Property.ValueChangeListener() {
+
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				// Retrieve the currently selected item
+				Item currItem = ls1.getItem(ls1.getValue());
+				// Retrieve the properties of the item
+				l1.setValue("ID: "
+						+ currItem.getItemProperty("identifier").getValue());
+				l2.setValue("Project name: "
+						+ currItem.getItemProperty("name").getValue());
+				l3.setValue("Description: "
+						+ currItem.getItemProperty("description").getValue());
+				l4.setValue("Members: "
+						+ currItem.getItemProperty("members").getValue());
+
 			}
 		});
-		layout.addComponent(button);
+
+		// Define root layout
+		final VerticalLayout layout = new VerticalLayout();
+		setContent(layout);
+		// Add components to layout layout
+		layout.setMargin(true);
+		layout.addComponent(ls1);
+		layout.addComponent(l1);
+		layout.addComponent(l2);
+		layout.addComponent(l3);
+		layout.addComponent(l4);
 	}
 
 }
